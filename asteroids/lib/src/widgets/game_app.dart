43b7f75cd2289b3218ffa2617441d481c3d3e8c1
NicolenaStiles <nicolena.stiles@gmail.com>
2024-02-14 03:11:39 -0500

    Peep this new ui ya nerds

--- asteroids/lib/src/widgets/game_app.dart
+++ asteroids/lib/src/widgets/game_app.dart
@@ -2,6 +2,8 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import '../asteroids.dart';
+import '../widgets/main_menu.dart';
+import '../widgets/tutorial.dart';
 
 import '../theme.dart';
 
@@ -28,80 +30,13 @@ class _GameAppState extends State<GameApp> {
         body: GameWidget.controlled(
           gameFactory: Asteroids.new,
           overlayBuilderMap: { 
-            PlayState.background.name: (context, game) =>
-              Center(
-                child: Text("animating background... "),
-              ),
+
             PlayState.mainMenu.name: (BuildContext context, Asteroids game) => 
-              Center ( 
-                child: Container( 
-                  constraints: const BoxConstraints(
-                    minWidth: 375,
-                    maxWidth: 512,
-                    minHeight: 375,
-                    maxHeight: 512,
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Column( 
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [ 
+              MainMenu(game: game),
+
+            PlayState.tutorial.name: (BuildContext context, Asteroids game) =>
+              Tutorial(game: game),
               
-                        Column( 
-                          mainAxisSize: MainAxisSize.min,
-                          children: [ 
-                          Text('(definitely not)',
-                            style: Theme.of(context).textTheme.headlineSmall),
-
-                          Text('ASTEROIDS',
-                            style: Theme.of(context).textTheme.headlineLarge),
-                          ],
-                        ),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [ 
-
-                          // start button
-                          OutlinedButton(
-                            onPressed: () {
-                              game.playState = PlayState.background; 
-                            },
-                            style: const ButtonStyle(
-                              padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
-                              side: MaterialStatePropertyAll(
-                                BorderSide(
-                                  color: Colors.white, 
-                                  width: 2))),
-                            child: 
-                              Text('start game',
-                                style: Theme.of(context).textTheme.titleMedium),
-                              ),
-
-                          // leaderboard 
-                          OutlinedButton(
-                            onPressed: () {}, 
-                            style: const ButtonStyle(
-                              padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
-                              side: MaterialStatePropertyAll(
-                                BorderSide(
-                                  color: Colors.white, 
-                                  width: 2))),
-                            child: 
-                              Text('leaderboard',
-                                style: Theme.of(context).textTheme.titleMedium),
-                              ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-            PlayState.tutorial.name: (context, game) =>
-              Center( 
-                child: Text("this is the tutorial menu"),
-              ),
             PlayState.gameOver.name: (context, game) =>
               Center( 
                 child: Text("this is the game over screen"),
